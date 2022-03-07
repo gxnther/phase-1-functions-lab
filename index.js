58c94078c1069165d99d97d5524dac04f25b0f3b
Gunther <gunt.valentine@gmail.com>
2022-03-07 18:24:17 -0500

    commit tree f111ed4afe1e30d7202ecd5a40d0ff1c35215664

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks > 42) {
+    return blocks - 42;
+} else {
+    return 42 - blocks }
+}
+
+function distanceFromHqInFeet(blocks) {
+   return distanceFromHqInBlocks(blocks) * 264;
+}
+distanceFromHqInFeet(1);
+
+function distanceTravelledInFeet(start, destination) {
+    let blocksInFeet;
+    blocksInFeet = (destination - start) * 264;
+    return Math.abs(blocksInFeet);
+}
+distanceTravelledInFeet();
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+    return 0;
+       }   else 
+        if (distanceTravelledInFeet(start, destination) > 400 &&
+             distanceTravelledInFeet(start, destination) < 2000) {
+            return (distanceTravelledInFeet(start, destination) - 400) *.02
+        }   else 
+        if (distanceTravelledInFeet(start, destination) > 2000 &&
+        distanceTravelledInFeet(start, destination) < 2500) {
+            return 25;
+        }   else
+        if (distanceTravelledInFeet(start, destination) > 2500) {
+                return `cannot travel that far`
+            }
+        }
+    
+
+calculatesFarePrice();
